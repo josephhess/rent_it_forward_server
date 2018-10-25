3fc54498e073ff60513bf10671b15ee608f2af4d
josephhess <readysteadyco@gmail.com>
2018-10-25 09:26:17 -0700

    cleanup and move jwt secret to .env, change the secret as it was committed to github openly

--- config.js
+++ config.js
@@ -3,11 +3,9 @@
 require('dotenv').config();
 
 module.exports = {
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY,
   PORT: process.env.PORT || 8080,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/rent',
-  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/rent-test',
-  JWT_SECRET: '1234THREE888Q',
-  JWT_EXPIRY: '1296000'
-
-
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/rent-test'
 };
